bb91b1f2e4c76f5290e877b53c68dddd1ceba6f5
StaceyCeleste <staceyfayers@yahoo.com>
2018-04-25 09:50:52 +0200

    License Key Generator
    First C# project - License Key Generator

---
+++ LicenseKeyGen/LicenseKeyGen/ClassFunctions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseKeyGen
+{
+    class ClassFunctions
+    {
+
+        public static string GenerateLicKeys()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var stringChars = new char[9];
+            var random = new Random();
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (i == 4)
+                {
+                    stringChars[i] = '-';
+                    continue;
+                }
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+            var finalString = new string(stringChars);
+            return finalString;
+        }
+
+    }
+}
---
+++ LicenseKeyGen/LicenseKeyGen/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace LicenseKeyGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.LicKeyBox = new System.Windows.Forms.TextBox();
+            this.GenerateBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LicKeyBox
+            // 
+            this.LicKeyBox.Enabled = false;
+            this.LicKeyBox.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LicKeyBox.ForeColor = System.Drawing.Color.Black;
+            this.LicKeyBox.Location = new System.Drawing.Point(58, 62);
+            this.LicKeyBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.LicKeyBox.Name = "LicKeyBox";
+            this.LicKeyBox.Size = new System.Drawing.Size(202, 25);
+            this.LicKeyBox.TabIndex = 0;
+            this.LicKeyBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // GenerateBtn
+            // 
+            this.GenerateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenerateBtn.Location = new System.Drawing.Point(83, 109);
+            this.GenerateBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.GenerateBtn.Name = "GenerateBtn";
+            this.GenerateBtn.Size = new System.Drawing.Size(152, 25);
+            this.GenerateBtn.TabIndex = 1;
+            this.GenerateBtn.Text = "Generate";
+            this.GenerateBtn.UseVisualStyleBackColor = true;
+            this.GenerateBtn.Click += new System.EventHandler(this.GenerateBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Head Office V2 License Key Generator";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 154);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GenerateBtn);
+            this.Controls.Add(this.LicKeyBox);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Head Office V2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LicKeyBox;
+        private System.Windows.Forms.Button GenerateBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
